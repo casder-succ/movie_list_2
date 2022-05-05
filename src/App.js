c0b38a8ba15777e1ae35f7759c2b075d8d5b4720
casder <casderiopus@gmail.com>
2022-05-05 23:56:14 +0300

    feat: get rid of css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import {createGlobalStyle} from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
 
 function App() {
     return (
-        <div>
-
-        </div>
+        <>
+            <GlobalStyles/>
+        </>
     );
 }
 
